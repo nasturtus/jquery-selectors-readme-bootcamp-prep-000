cef678c7179ad3a2c63951fe71f5836535150368
Santosh SN <nasturtus@gmail.com>
2018-02-10 02:26:39 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,7 +4,7 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
- return $('#pic-list ul:last-child') 
+   return $('.pics #pic-list li:last-child')
 }
 
 function ninjaBabySelector() {
